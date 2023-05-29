1746d5c576718a5f4ebadc1b094d897951830108
StuSerious <stefano.serioli@outlook.com>
2023-05-29 19:07:57 +0200

    fix: remove unused settings

--- src/threedy/modules/settings.py
+++ src/threedy/modules/settings.py
@@ -1,23 +1,15 @@
 # size
-APP_SIZE = (1280, 720)
-IS_RESIZEABLE = False
-
-# grid
-MAIN_ROWS = 4
-MAIN_COLS = 4
+APP_SIZE = {"width": 1280, "height": 720}
 
 # text
 FONT = "Calibri"
 FONT_SIZE = 14
 FONT_WEIGHT = ("normal", "bold")
 
-# terminal
-TERMINAL_BG_COLOR = "#000000"
-TERMINAL_TEXT_COLOR = "#90cf00"
-
 # styling
 PADDING = {"none": 0, "medium": 10, "large": 20}
 STYLING = {"gap": 0.5, "corner-radius": 5}
 
-# colors
-COLORS = {}
+# terminal
+TERMINAL_BG_COLOR = "#000000"
+TERMINAL_TEXT_COLOR = "#90cf00"
