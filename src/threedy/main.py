7fd9c3cb9f28ecff9c5bdd68777d2dd211fb49e3
StuSerious <stefano.serioli@outlook.com>
2023-05-31 14:36:59 +0200

    chore: remove unneeded docstring

--- src/threedy/main.py
+++ src/threedy/main.py
@@ -14,20 +14,10 @@ from modules.settings import *
 # PyInstaller helper
 # https://stackoverflow.com/questions/7674790/bundling-data-files-with-pyinstaller-onefile/13790741#13790741
 def resource_path(relative_path):
-    """Get absolute path to resource, works for dev and for PyInstaller
-
-    Args:
-        relative_path (string): the relative path of the resource
-
-    Returns:
-        string: the base pass joined with the relative path
-    """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
+    try:  # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.abspath(".")
-
     return os.path.join(base_path, relative_path)
 
 
