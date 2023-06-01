fd11cc6e0201520913d7a890246c272741a3a91f
StuSerious <stefano.serioli@outlook.com>
2023-06-01 11:58:56 +0200

    chore: update pyinstaller onefile helper

--- src/threedy/main.py
+++ src/threedy/main.py
@@ -11,13 +11,13 @@ from modules.dialogs import export_file_dialog, select_file_dialog
 from modules.settings import *
 
 
-# PyInstaller helper
-# https://stackoverflow.com/questions/7674790/bundling-data-files-with-pyinstaller-onefile/13790741#13790741
+# PyInstaller `--onefile` helper
+# https://github.com/TomSchimansky/CustomTkinter/discussions/939
 def resource_path(relative_path):
-    try:  # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
+    base_path = getattr(
+        sys,
+        '_MEIPASS',
+        os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
 
