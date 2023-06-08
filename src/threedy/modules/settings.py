cc4eb1a27de3ed6672091eb77ff6ca8422a9f6a2
StuSerious <stefano.serioli@outlook.com>
2023-06-08 15:06:06 +0200

    settings: file reformat

--- src/threedy/modules/settings.py
+++ src/threedy/modules/settings.py
@@ -1,28 +1,31 @@
+# ------------------------------------------------------------------------------------ #
+#                                   threedy settings                                   #
+# ------------------------------------------------------------------------------------ #
+
 # ------------------------------------- app info ------------------------------------- #
+
 VERSION = "0.1.0-alpha.6"
 
+
 # ---------------------------------- window settings --------------------------------- #
-APP_SIZE = {
-    "width": 900,
-    "height": 500,
-}
-PADDING = {
-    "none": 0,
-    "small": 5,
-    "medium": 10,
-    "large": 20,
-}
+
+APP_SIZE = {"width": 900, "height": 500}
+
+PADDING = {"none": 0, "small": 5, "medium": 10, "large": 20}
+
 
 # --------------------------------------- fonts -------------------------------------- #
+
 FONT = "Calibri"
 FONT_SIZE = 14
-# terminal
 TERMINAL_FONT = "Fira Code"
 TERMINAL_FONT_SIZE = 13
 TERMINAL_BG_COLOR = "#000000"
 TERMINAL_TEXT_COLOR = "#90cf00"
 
+
 # --------------------------------------- regex -------------------------------------- #
+
 RE_PATTERNS = {
     "remove_comments": r"\s*;.*$",
     "remove_mcodes": r"^M.*$",
@@ -30,4 +33,4 @@ RE_PATTERNS = {
     "remove_nontravel": r"^(?!G[10]).*$[\r\n]*",
     "remove_lone_gs": r"^[G10]+$[\r\n]*",
     "remove_coordname": r"G([01])\s+X(\S+)\s+Y(\S+)(?:\s+Z(\S+))?(\n|$)",
-}
+}   
